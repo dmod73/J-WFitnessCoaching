169bd767237bfcb1836173986a196616839707fa
dmod73 <ortizdiazdeanm@gmail.com>
2025-10-05 18:31:26 -0400

    chore: initialize stable Next.js (App Router) skeleton

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+﻿export default function Page() {
+  return (
+    <main>
+      <div>
+        <h1>J-WFitnessCoaching</h1>
+        <p>Base Next.js lista. Próximo paso: Fase 1 (Usuarios + Roles).</p>
+      </div>
+    </main>
+  );
+}
